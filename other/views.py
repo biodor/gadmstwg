483b11d321f733e3657c7f6c7e0c164c481f8067
biodor <biodorb@gmail.com>
2022-09-28 17:12:11 +0800

    Update views.py

--- other/views.py
+++ other/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
 
 def simple_view(request):
-    return HttpRequest('Hello!')
+    return HttpResponse('Hello!')
 
 # Create your views here.
